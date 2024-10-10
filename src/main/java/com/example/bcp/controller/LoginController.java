0eea82fe7d410e544032c7799b87a8ee8c61bc3c
Seriendipity <1378834271@qq.com>
2024-10-10 18:04:12 +0800

    修改了Login的返回值，返回类型为一个Result对象

--- src/main/java/com/example/bcp/controller/LoginController.java
+++ src/main/java/com/example/bcp/controller/LoginController.java
@@ -1,27 +1,40 @@
 package com.example.bcp.controller;
 
+import com.example.bcp.entity.Result;
 import com.example.bcp.service.StudentService;
 import com.example.bcp.utils.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.converter.json.GsonBuilderUtils;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.swing.*;
+import java.util.Map;
 
 @RestController
 public class LoginController {
     @Autowired
     private StudentService studentService;
 
-    @RequestMapping("/login")
-    public String login(String userName , String password){
-        if(studentService.selectByStudentNo(userName) == null ||
-                !studentService.selectByStudentNo(userName).getPassword().equals(password)
-        ){
-            System.out.println("用户名或密码错误");
-            return "";
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public Result login(@RequestBody Map<String, String> loginData) {
+        String userName = loginData.get("username");
+        String password = loginData.get("password");
+
+        System.out.println(userName);
+        System.out.println(password);
+        if (studentService.selectByStudentNo(userName) == null ||
+                !studentService.selectByStudentNo(userName).getPassword().equals(password)) {
+            System.out.println(userName);
+            System.out.println(password);
+            return Result.error("用户名或密码错误") ;
         }
-        String token = JwtUtils.createToken(userName,password);
-        return token;
+
+        String token = JwtUtils.createToken(userName, password);
+        return Result.success(token);
     }
+
 }
