bd1ec42a3451da765cfc2601bc9222caab83eaff
Tianyi Jiang <1982581695@qq.com>
2024-11-15 16:05:08 +0800

    modify

--- src/views/Bookmarks/bemarkedposts.vue
+++ src/views/Bookmarks/bemarkedposts.vue
@@ -1,12 +1,12 @@
 <template>
-    <el-container>
-      <el-header class="head">
-        <el-row :gutter="20">
-            <el-col :span="18">
+  <el-container>
+    <el-header class="head">
+      <el-row :gutter="20">
+        <el-col :span="18">
           <div class="grid-content">
-            <router-link to="/firstpage" style="text-decoration: none; " >
-            <h1 class="zitihead">智慧课程平台</h1>
-          </router-link>
+            <router-link to="/firstpage" style="text-decoration: none; ">
+              <h1 class="zitihead">智慧课程平台</h1>
+            </router-link>
           </div>
         </el-col>
         <el-col :span="1">
@@ -17,88 +17,94 @@
             </router-link>
           </div>
         </el-col>
-          <el-col :span="2">
-            <div class="grid-content ">
-              <router-link to="/firstpage" style="text-decoration: none;">
-                <h1 style="font-size:x-large;margin-top: 17px;color: white;text-shadow: 2px 3px rgba(0, 0, 0, 0.5);font-weight: 550;">收藏夹
-                </h1>
-              </router-link>
-            </div>
-          </el-col>
-          <el-col :span="1">
-            <div class="grid-content ">
-              <router-link to="/myinformation" style="text-decoration: none;">
-                <div style="margin-top: 10px"><el-avatar :size="40" :src="circleUrl"></el-avatar></div>
-              </router-link>
-            </div>
-          </el-col>
-          <el-col :span="2">
-            <router-link to="/myinformation" style="text-decoration: none;">
-              <h1 style="font-size: medium;margin-top: 21px;color: aliceblue;font-weight: 550;">爱学习
+        <el-col :span="2">
+          <div class="grid-content ">
+            <router-link to="/firstpage" style="text-decoration: none;">
+              <h1
+                style="font-size:x-large;margin-top: 17px;color: white;text-shadow: 2px 3px rgba(0, 0, 0, 0.5);font-weight: 550;">
+                收藏夹
               </h1>
             </router-link>
-          </el-col>
-        </el-row>
-      </el-header>
-  
-      <el-main class="backmain1">
-        <el-container>
-          <el-aside class="backleft">
-            <div class="whiteback" :data="student">
-              <h1 style="text-align: left; font-weight: bold;margin-bottom: 5px;">个人信息</h1>
-              <div style="margin-top: 20px;text-align: center; "><el-avatar :size="100" :src="circleUrl"></el-avatar></div>
-              <h1 class="ziti01">学生</h1>
-              <h1 class="ziti02" style="text-align: left;padding-left: 15px;">姓名：{{ student.studentName }}</h1>
-              <h1 class="ziti02" style="text-align: left;padding-left: 15px;">学号：{{ student.studentNo }}</h1>
-              <h1 class="ziti02" style="text-align: left;padding-left: 15px;">学院：{{ student.dept }}</h1>
-              <h1 class="ziti02" style="text-align: left;padding-left: 15px;">邮箱：{{ student.email }}</h1>
+          </div>
+        </el-col>
+        <el-col :span="1">
+          <div class="grid-content ">
+            <router-link to="/myinformation" style="text-decoration: none;">
+              <div style="margin-top: 10px"><el-avatar :size="40" :src="circleUrl"></el-avatar></div>
+            </router-link>
+          </div>
+        </el-col>
+        <el-col :span="2">
+          <router-link to="/myinformation" style="text-decoration: none;">
+            <h1 style="font-size: medium;margin-top: 21px;color: aliceblue;font-weight: 550;">爱学习
+            </h1>
+          </router-link>
+        </el-col>
+      </el-row>
+    </el-header>
+
+    <el-main class="backmain1">
+      <el-container>
+        <el-aside class="backleft">
+          <div class="whiteback" :data="student">
+            <h1 style="text-align: left; font-weight: bold;margin-bottom: 5px;">个人信息</h1>
+            <div style="margin-top: 20px;text-align: center; "><el-avatar :size="100" :src="circleUrl"></el-avatar>
             </div>
-            <div class="whiteback2">
-              <h1 style="text-align: left; font-weight: bold;margin-bottom: 5px;">课程提醒</h1>
-            </div>
-          </el-aside>
-  
-          <el-main height="600px">
-            <el-row>
+            <h1 class="ziti01">学生</h1>
+            <h1 class="ziti02" style="text-align: left;padding-left: 15px;">姓名：{{ student.studentName }}</h1>
+            <h1 class="ziti02" style="text-align: left;padding-left: 15px;">学号：{{ student.studentNo }}</h1>
+            <h1 class="ziti02" style="text-align: left;padding-left: 15px;">学院：{{ student.dept }}</h1>
+            <h1 class="ziti02" style="text-align: left;padding-left: 15px;">邮箱：{{ student.email }}</h1>
+          </div>
+          <div class="whiteback2">
+            <h1 style="text-align: left; font-weight: bold;margin-bottom: 5px;">课程提醒</h1>
+          </div>
+        </el-aside>
+
+        <el-main height="600px">
+          <el-row>
             <el-col :span="3">
               <router-link to="/bookmarks" style="text-decoration: none;">
-              <el-button type="primary" style="text-align: left; font-weight: bold;font-size: large;width: 100px;" plain>收藏夹</el-button>
+                <el-button type="primary" style="text-align: left; font-weight: bold;font-size: large;width: 100px;"
+                  plain>收藏夹</el-button>
               </router-link>
             </el-col>
             <el-col :span="21">
               <router-link to="/othersposts" style="text-decoration: none;">
-                <el-button type="primary" style="text-align: left; font-weight: bold;font-size: large;width: 100px;" >帖子</el-button>
+                <el-button type="primary"
+                  style="text-align: left; font-weight: bold;font-size: large;width: 100px;">帖子</el-button>
               </router-link>
             </el-col>
           </el-row>
-            <!-- <div class="scrollable"> -->
-                <div class="grid-content bg-white" style="padding-left: 10px;" v-for="post in posts" :key="post.postNo">
+          <!-- <div class="scrollable"> -->
+          <div class="grid-content bg-white" style="padding-left: 10px;" v-for="post in posts" :key="post.postNo">
             <el-row :gutter="20">
               <el-col :span="21">
-                <h1 class="ziti03" style="margin-top: 5px;">{{ post.postLesson }}  </h1>
+                <h1 class="ziti03" style="margin-top: 5px;">{{ post.postLesson }} </h1>
                 <h1 class="ziti04" style="line-height: 1.5"> {{ post.postInfo }} </h1>
-                <h1 class="ziti04" style="color: gray;margin-top: 15px;margin-bottom: 15px;">{{ post.uploadDate }}   {{ post.postAuthor }}  上传 </h1>
+                <h1 class="ziti04" style="color: gray;margin-top: 15px;margin-bottom: 15px;">{{ post.uploadDate }} {{
+                  post.postAuthor }} 上传 </h1>
               </el-col>
-              
+
               <el-col :span="3">
                 <el-button type="primary" style="margin-top: 10px;" @click="updatepost(post)">取消收藏</el-button>
               </el-col>
             </el-row>
           </div>
-        
-          </el-main>
-  
-        </el-container>
-      </el-main>
-    </el-container>
-  </template>
-  
-  <script>
-  import { ref, onMounted } from 'vue';
+
+        </el-main>
+
+      </el-container>
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+import { ref, onMounted } from 'vue';
 import { useRouter } from 'vue-router';
 import { reqUpdateVisible } from '@/api/api'; // 假设这是更新帖子权限状态的API
 import { ElNotification } from 'element-plus';
-import { reqUserInfo,reqCourseList } from '@/api/api';
+import { reqUserInfo, reqCourseList } from '@/api/api';
 
 export default {
   setup() {
@@ -126,7 +132,7 @@ export default {
         postNo: 3,
         postLesson: 'UI前端',
         postInfo: 'line-height的值应该大于或等于文本的字体大小，以确保行间距生效。如果文本的字体大小小于line-height的值，行间距将不会显示出来。如果你的文本字体大小小于1.25或1.5，你可能需要调整line-height的值或字体大小以得到期望的行间距效果。',
-        sendStatus: true ,
+        sendStatus: true,
         postAuthor: '张学琛',
         uploadDate: '2024-11-11',
         authority: true,
@@ -134,12 +140,12 @@ export default {
       // 更多帖子数据...
     ]);
     const mockData = {
-  studentName: '张三',
-  studentNo: '20220001',
-  dept: '计算机科学与技术',
-  email: 'zhangsan@example.com',
-  avatarUrl: 'src/assets/images/example.jpg'
-};
+      studentName: '张三',
+      studentNo: '20220001',
+      dept: '计算机科学与技术',
+      email: 'zhangsan@example.com',
+      avatarUrl: 'src/assets/images/example.jpg'
+    };
     const posts = ref([]);//界面展示的
     const student = ref([]);
     const router = useRouter(); // 获取路由实例
@@ -164,33 +170,33 @@ export default {
     const updatePostFavStatus = async (post) => {
       // 更新收藏状态
       try {
-         await reqUpdateVisible({
-           postNo: post.postNo,
-         });
-       } catch (error) {
-          console.error('取消收藏失败:', error);
-          ElNotification({
-            type: 'error',
-            message: '取消收藏失败，请重试。',
-         });
-       }
-     };
+        await reqUpdateVisible({
+          postNo: post.postNo,
+        });
+      } catch (error) {
+        console.error('取消收藏失败:', error);
+        ElNotification({
+          type: 'error',
+          message: '取消收藏失败，请重试。',
+        });
+      }
+    };
 
     onMounted(async () => {
-  try {
-    const userResponse = await reqUserInfo();
-    const postResponse = await reqPostList();
-    student.value = userResponse.data;
-    posts.value = postResponse.data;
-  } catch (error) {
-    student.value = mockData;
-    posts.value = mockPost.value;
-    ElNotification({
-      type: 'error',
-      message: '获取信息失败',
+      try {
+        const userResponse = await reqUserInfo();
+        const postResponse = await reqPostList();
+        student.value = userResponse.data;
+        posts.value = postResponse.data;
+      } catch (error) {
+        student.value = mockData;
+        posts.value = mockPost.value;
+        ElNotification({
+          type: 'error',
+          message: '获取信息失败',
+        });
+      }
     });
-  }
-});
 
     return {
       posts,
@@ -204,136 +210,136 @@ export default {
     };
   },
 };
-  </script>
+</script>
 
-  
-  
-  <style>
-  .head {
-    background-color: #005bac;
-  
+
+
+<style>
+.head {
+  background-color: #005bac;
+
+}
+
+.backmain1 {
+  height: 770px;
+  background-color: #eaf6ff;
+}
+
+.zitihead {
+  text-align: left;
+  font-weight: bold;
+  font-size: 24px;
+  margin-top: 15px;
+  color: #ffffff;
+}
+
+.ziti01 {
+  text-align: center;
+  font-weight: 400;
+  font-size: 14px;
+  margin-bottom: 25px;
+  margin-top: 10px;
+}
+
+.ziti02 {
+  text-align: center;
+  font-weight: bold;
+  font-size: 16px;
+}
+
+.ziti03 {
+  text-align: left;
+  font-weight: bold;
+  margin-bottom: 5px;
+  font-size: 16px;
+  margin-left: 10px;
+  padding-top: 10px;
+}
+
+.ziti04 {
+  text-align: left;
+  font-weight: 400;
+  margin-bottom: 2px;
+  font-size: 14px;
+  margin-left: 10px;
+}
+
+.backleft {
+  width: "350px";
+  max-height: "550px";
+  background-color: #eaf6ff;
+}
+
+.backright {
+  width: "350px";
+  height: "600px";
+  background-color: #eaf6ff;
+}
+
+
+.whiteback {
+  border-radius: 4px;
+  max-height: 350px;
+  background-color: #ffffff;
+  padding: 10px;
+  box-shadow: 0 2px 12px rgba(0, 0, 0, 0.1);
+
+}
+
+.whiteback2 {
+  margin-top: 20px;
+  border-radius: 4px;
+  height: 200px;
+  background-color: #ffffff;
+  padding: 10px;
+  box-shadow: 0 2px 12px rgba(0, 0, 0, 0.1);
+}
+
+.whiteback3 {
+  /* margin-top: 20px; */
+  border-radius: 4px;
+  height: 400px;
+  background-color: #ffffff;
+  padding: 10px;
+  box-shadow: 0 2px 12px rgba(0, 0, 0, 0.1);
+}
+
+.scrollable {
+  height: 690px;
+  /* 固定高度 */
+  overflow-y: auto;
+  /* 垂直方向上的滚动条 */
+  overflow-x: hidden;
+  padding-right: 10px;
+}
+
+.el-row {
+  margin-bottom: 20px;
+
+  &:last-child {
+    margin-bottom: 0;
   }
-  
-  .backmain1 {
-    height: 770px;
-    background-color: #eaf6ff;
-  }
-  
-  .zitihead {
-    text-align: left;
-    font-weight: bold;
-    font-size: 24px;
-    margin-top: 15px;
-    color: #ffffff;
-  }
-  
-  .ziti01 {
-    text-align: center;
-    font-weight: 400;
-    font-size: 14px;
-    margin-bottom: 25px;
-    margin-top: 10px;
-  }
-  
-  .ziti02 {
-    text-align: center;
-    font-weight: bold;
-    font-size: 16px;
-  }
-  
-  .ziti03 {
-    text-align: left;
-    font-weight: bold;
-    margin-bottom: 5px;
-    font-size: 16px;
-    margin-left: 10px;
-    padding-top: 10px;
-  }
-  
-  .ziti04 {
-    text-align: left;
-    font-weight: 400;
-    margin-bottom: 2px;
-    font-size: 14px;
-    margin-left: 10px;
-  }
-  
-  .backleft {
-    width: "350px";
-    max-height: "550px";
-    background-color: #eaf6ff;
-  }
-  
-  .backright {
-    width: "350px";
-    height: "600px";
-    background-color: #eaf6ff;
-  }
-  
-  
-  .whiteback {
-    border-radius: 4px;
-    max-height: 350px;
-    background-color: #ffffff;
-    padding: 10px;
-    box-shadow: 0 2px 12px rgba(0, 0, 0, 0.1);
-  
-  }
-  
-  .whiteback2 {
-    margin-top: 20px;
-    border-radius: 4px;
-    height: 200px;
-    background-color: #ffffff;
-    padding: 10px;
-    box-shadow: 0 2px 12px rgba(0, 0, 0, 0.1);
-  }
-  
-  .whiteback3 {
-    /* margin-top: 20px; */
-    border-radius: 4px;
-    height: 400px;
-    background-color: #ffffff;
-    padding: 10px;
-    box-shadow: 0 2px 12px rgba(0, 0, 0, 0.1);
-  }
-  
-  .scrollable {
-    height: 690px;
-    /* 固定高度 */
-    overflow-y: auto;
-    /* 垂直方向上的滚动条 */
-    overflow-x: hidden;
-    padding-right: 10px;
-  }
-  
-  .el-row {
-    margin-bottom: 20px;
-  
-    &:last-child {
-      margin-bottom: 0;
-    }
-  }
-  
-  .el-col {
-    border-radius: 4px;
-  }
-  
-  .bg-white {
-    background: #ffffff;
-    box-shadow: 0 2px 12px rgba(0, 0, 0, 0.1);
-    border-radius: 30px
-  }
-  
-  .grid-content {
-    border-radius: 4px;
-    min-height: 36px;
-    /* height: 250px; */
-    margin-bottom: 10px;
-  
-  }
-  
-  body>.el-container {
-    margin-bottom: 40px;
-  }
-  </style>
+}
+
+.el-col {
+  border-radius: 4px;
+}
+
+.bg-white {
+  background: #ffffff;
+  box-shadow: 0 2px 12px rgba(0, 0, 0, 0.1);
+  border-radius: 30px
+}
+
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+  /* height: 250px; */
+  margin-bottom: 10px;
+
+}
+
+body>.el-container {
+  margin-bottom: 40px;
+}
+</style>
