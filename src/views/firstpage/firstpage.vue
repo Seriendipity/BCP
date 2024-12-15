53bcc7454da4b3166d2a63daca7dcaa8c4c02895
AmyLiu <1062628601@qq.com>
2024-12-15 18:24:41 +0800

    vue

--- src/views/firstpage/firstpage.vue
+++ src/views/firstpage/firstpage.vue
@@ -87,7 +87,7 @@
             <div style="margin-top: 20px;text-align: center; "><el-avatar :size="100"
                 :src="userInfo.avatarUrl"></el-avatar>
             </div>
-            <h1 class="ziti01">学生</h1>
+            <h1 class="ziti01">{{ userInfo.userIdentity }}</h1>
             <h1 class="ziti02" style="text-align: left;padding-left: 15px;">姓名：{{ userInfo.userName }}</h1>
             <h1 class="ziti02" style="text-align: left;padding-left: 15px;">学号：{{ userInfo.userId }}</h1>
             <h1 class="ziti02" style="text-align: left;padding-left: 15px;">学院：{{ userInfo.dept }}</h1>
@@ -298,6 +298,7 @@ const mockDataHomework = [
 
 const mockDataUser = {
   userName: '张三',
+  userIdentity:'学生',
   userId: 'T20220001',
   dept: '计算机科学与技术',
   email: 'zhangsan@example.com',
